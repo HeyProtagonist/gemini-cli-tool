59a18d5cc0c0c74794a637c8f4e08efc867d051b
Anguram Shanmugam <anguram.k.s@gmail.com>
2024-03-31 16:52:17 +0530

    Good First Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+require("dotenv").config(); // get `.env` vars (Shouldn't work like this 🤦)
+const readline = require("node:readline");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const printToConsole = require("./printToConsole");
+
+const gemini = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const IO = { input: process.stdin, output: process.stdout };
+const rl = readline.createInterface(IO);
+const history = [];
+
+const UP_NEXT = "\nBring It On Buddy 👋 😏 🫴 ...\n\n";
+const HELLO_FRIEND = "\nHello Old Friend 👋, What's My Itinerary..?\n\n";
+
+(async function analysis() {
+  const genAI = gemini.getGenerativeModel({ model: "gemini-pro" });
+  const question = history.length ? UP_NEXT : HELLO_FRIEND;
+
+  rl.question(question, async (query) => {
+    const result = await genAI.generateContent(query);
+    const response = result.response.text();
+
+    history.push({ user: query, gemini: response });
+    printToConsole(response);
+
+    analysis(); // Loops back to next question
+  });
+})();
